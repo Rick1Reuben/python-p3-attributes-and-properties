9e82bd3a3b86da2f594c13bd3d0a00766d9ca098
Reuben Kamau <rickr6129@gmail.com>
2024-05-27 21:26:13 +0300

    another lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,13 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="", breed=""):
+        if not isinstance(name, str) or len(name) == 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            return
+        self.name = name
+        
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            return
+        self.breed = breed
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,13 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="", job=""):
+        if not isinstance(name, str) or len(name) == 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            return
+        self.name = name.title()
+
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+            return
+        self.job = job
